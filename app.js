664b34406cc7c08d85fbad35d39d14f947f3c9a4
jsbalrog <edwardtjenkins@gmail.com>
2014-08-13 08:32:13 -0600

    Finished up app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // ------- Require dependencies --------
 var express = require('express')
+	, routes = require('./routes')
 	, http = require('http')
 	, path = require('path')
 	, mongoskin = require('mongoskin')
@@ -28,13 +29,44 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // ------- Define middleware --------
-// expose collections in each express route via the req object
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(methodOverride());
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+if('development' == app.get('env')) {
+	app.use(errorHandler());
+}
+
+// expose collections in each express route via the req object
+app.use(function(req, res, next) {
+	if(!collections.articles || !collections.users) return next(new Error('No collections.'));
+	req.collections = collections;
+	return next();
+});
 
 // ------- Define routes --------
+app.get('/', routes.index);
+app.get('/login', routes.user.login);
+app.post('/login', routes.user.authenticate);
+app.get('/logout', routes.user.logout);
+app.get('/admin', routes.article.admin);
+app.get('/post', routes.article.post);
+app.post('/post', routes.article.postArticle);
+app.get('/articles/:slug', routes.article.show);
+
+// REST API routes
+app.get('/api/articles', routes.article.list);
+app.post('/api/articles', routes.article.add);
+app.put('/api/articles/:id', routes.article.edit);
+app.del('/api/articles/:id', routes.article.del);
+
+
+// Catch-all
 app.all('*', function(req, res) {
-	// Render a view: res.render(viewName, <data>, <callback(error, html)>)
-	res.render('index', { msg: 'Welcome' });
+	res.send(404);
 });
 
 // ------- Start the server --------
---
+++ routes/user.js
@@ -0,0 +1,15 @@
+exports.list = function(req, res) {
+	res.send('respond with a resource');
+};
+
+exports.login = function(req, res, next) {
+	res.render('login');
+};
+
+exports.logout = function(req, res, next) {
+	res.redirect('/');
+};
+
+exports.authenticate = function(req, res, next) {
+	res.redirect('/admin');
+};
